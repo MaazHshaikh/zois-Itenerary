cdd0642e0ef25407faaef3ef90a1a805cf7fa6c7
MaazHshaikh <91615848+MaazHshaikh@users.noreply.github.com>
2023-12-29 15:51:33 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import { useEffect, useState } from "react";
+
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import CityList from "./components/Citylist";
+import City from "./components/City";
+import Form from "./components/Form";
+import Login from "./pages/Login";
+import CountryList from "./components/CountryList";
+import { CitiesProvider } from "./contexts/CitiesContext";
+
+function App() {
+  return (
+    <CitiesProvider>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </CitiesProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,56 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
+function Map() {
+  const navigate = useNavigate();
+  const { cities } = useCities();
+  const [mapPosition, setMapPostion] = useState([40, 0]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPostion([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
+
+  return (
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+      </MapContainer>
+    </div>
+  );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+export default Map;
